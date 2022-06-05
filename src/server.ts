08f45b6de3a8a6f5fafb22321de769224c6ab0cf
ahmadelzohairy <ahmadelzohairy93@gmail.com>
2022-06-05 15:06:01 +0200

    add middlewares for logging , secuirty and parsing body

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
 import express, {Request, Response} from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 //port
 const PORT =3000;
 //create server instance
 const app = express();
 
+//middlewares
+app.use(morgan('common'));
+app.use(helmet());
+
+
 //route
 app.get('/', (req: Request, res: Response)=>{
     res.json({
         msg: 'helloworld'
     })
+})
+//post request
+app.post('/', express.json(), (req: Request, res: Response) => {
+
+    res.json({
+        "message": "hellow world from post !!",
+        "data": req.body
+    })
+    console.log(req.body);  
 })
 
 //listen server on the port(start server)
